f3ff342c600332b3b8037e32294a5c1ff1c990fc
LeBalz <lebalz@outlook.com>
2024-08-05 00:42:47 +0200

    use latest doc-live-brython

--- src/theme/CodeEditor/WithScript/ScriptContext.tsx
+++ src/theme/CodeEditor/WithScript/ScriptContext.tsx
@@ -4,16 +4,30 @@ import { InitState } from 'docusaurus-live-brython/theme/CodeEditor/WithScript/T
 import Script, { ScriptMeta } from '@site/src/models/documents/Script';
 import { useFirstMainDocument } from '@site/src/hooks/useFirstMainDocument';
 import Loader from '@site/src/components/Loader';
+import CodeBlock from '@theme/CodeBlock';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 export const Context = React.createContext<Script | undefined>(undefined);
 
 const ScriptContext = observer((props: InitState & { children: JSX.Element }) => {
     const [meta] = React.useState(new ScriptMeta(props));
     const doc = useFirstMainDocument(props.id, meta);
 
-    if (!doc) {
-        return <Loader label="Code laden..." />;
-    }
-    return <Context.Provider value={doc}>{props.children}</Context.Provider>;
+    return (
+        <BrowserOnly fallback={<CodeBlock language={props.lang}>{props.code}</CodeBlock>}>
+            {() => {
+                if (!doc) {
+                    // return <Loader label="Code laden..." />;
+                    return (
+                        <div>
+                            <CodeBlock language={props.lang}>{props.code}</CodeBlock>
+                            <Loader label="Code laden..." overlay />
+                        </div>
+                    );
+                }
+                return <Context.Provider value={doc}>{props.children}</Context.Provider>;
+            }}
+        </BrowserOnly>
+    );
 });
 
 export default ScriptContext;
