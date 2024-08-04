1875879576cc40b4058525db701979d677896a47
LeBalz <lebalz@outlook.com>
2024-08-04 23:08:46 +0200

    add and display loader component

--- src/theme/CodeEditor/WithScript/ScriptContext.tsx
+++ src/theme/CodeEditor/WithScript/ScriptContext.tsx
@@ -3,16 +3,17 @@ import { observer } from 'mobx-react-lite';
 import { InitState } from 'docusaurus-live-brython/theme/CodeEditor/WithScript/Types';
 import Script, { ScriptMeta } from '@site/src/models/documents/Script';
 import { useFirstMainDocument } from '@site/src/hooks/useFirstMainDocument';
+import Loader from '@site/src/components/Loader';
 export const Context = React.createContext<Script | undefined>(undefined);
 
 const ScriptContext = observer((props: InitState & { children: JSX.Element }) => {
     const [meta] = React.useState(new ScriptMeta(props));
-    const document = useFirstMainDocument(props.id, meta);
+    const doc = useFirstMainDocument(props.id, meta);
 
-    if (!document) {
-        return <div>Load</div>;
+    if (!doc) {
+        return <Loader label="Code laden..." />;
     }
-    return <Context.Provider value={document}>{props.children}</Context.Provider>;
+    return <Context.Provider value={doc}>{props.children}</Context.Provider>;
 });
 
 export default ScriptContext;
