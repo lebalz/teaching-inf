57f48b4c587f446e480695c84e8124f18da96f6b
Silas Berger <silas.berger@outlook.com>
2024-08-08 08:55:17 +0200

    Show basic panel with root and shared access.

---
+++ src/components/PermissionsPanel/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import styles from './styles.module.scss';
+import { rootStore } from '@site/src/stores/rootStore';
+import Button from '../shared/Button';
+import { mdiShieldLockOutline } from '@mdi/js';
+import DocumentRoot from '@site/src/models/DocumentRoot';
+import Loader from '@site/src/components/Loader';
+
+interface Props {
+    documentRootId: string;
+}
+
+const PermissionsPanel = ({documentRootId}: Props) => {
+
+    const [documentRoot, setDocumentRoot] = useState<DocumentRoot<any> | null>();
+
+    const onOpen = async () => {
+        setDocumentRoot(rootStore.documentRootStore.find(documentRootId));
+    }
+
+    return (
+        <Popup
+            trigger={<Button icon={mdiShieldLockOutline}></Button>}
+            onOpen={onOpen}
+            modal
+            closeOnEscape
+            closeOnDocumentClick>
+
+            <div className={styles.popupContentContainer}>
+                <h2>Permissions</h2>
+                {!!documentRoot &&
+                    <div>
+                        <div>Root permission: {documentRoot.permission}</div>
+                        <div>Shared access: {documentRoot.sharedAccess}</div>
+                    </div>
+                }
+
+                {!documentRoot &&
+                    <Loader />
+                }
+            </div>
+        </Popup>
+    );
+};
+
+export default PermissionsPanel;
