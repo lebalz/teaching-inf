4a6e116621cb4ad65298673f540245e70e13568a
LeBalz <lebalz@outlook.com>
2024-08-09 17:10:22 +0200

    fix link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -144,7 +144,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/GBSL-Informatik',
+          href: 'https://github.com/GBSL-Informatik/teaching-dev',
           label: 'GitHub',
           position: 'right',
         },
