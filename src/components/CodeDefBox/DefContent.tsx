32df5a828be63b124210f8c925cc3947a7d9d791
LeBalz <lebalz@outlook.com>
2024-08-09 15:56:04 +0200

    format codebase

--- src/components/CodeDefBox/DefContent.tsx
+++ src/components/CodeDefBox/DefContent.tsx
@@ -3,17 +3,12 @@ import clsx from 'clsx';
 
 import styles from './styles.module.scss';
 
-
 interface Props {
     children: React.JSX.Element | React.JSX.Element[];
 }
 
 const DefContent = (props: Props) => {
-    return (
-        <div className={clsx(styles.content)}>
-            {props.children}
-        </div>
-    )
-}
+    return <div className={clsx(styles.content)}>{props.children}</div>;
+};
 
 export default DefContent;
