8c643119edece46116fa2a743289f88f7cd4c4f5
LeBalz <lebalz@outlook.com>
2024-08-09 15:50:14 +0200

    add code-defbox

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -2,9 +2,15 @@
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import DefinitionList from '../components/DefinitionList';
+import DefBox from '../components/CodeDefBox';
+import DefHeading from '../components/CodeDefBox/DefHeading';
+import DefContent from '../components/CodeDefBox/DefContent';
 
 export default {
     // Re-use the default mapping
     ...MDXComponents,
-    Dl: DefinitionList
+    Dl: DefinitionList,
+    DefBox: DefBox,
+    DefHeading: DefHeading,
+    DefContent: DefContent
 };
