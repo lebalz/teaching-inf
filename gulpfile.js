eea9aa2441204d580d494ac8933b53c1c7e4422e
LeBalz <lebalz@outlook.com>
2024-08-10 15:24:51 +0200

    format

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-const gulp = require('gulp')
-const connect = require('gulp-connect')
+const gulp = require('gulp');
+const connect = require('gulp-connect');
 
-gulp.task('reload', () => gulp.src(['static/p/**/*.html', 'static/p/**/*.md'])
-    .pipe(connect.reload()));
+gulp.task('reload', () => gulp.src(['static/p/**/*.html', 'static/p/**/*.md']).pipe(connect.reload()));
 
 gulp.task('serve', () => {
-
     connect.server({
         root: 'static/',
         port: 8001,
         host: '0.0.0.0',
         livereload: true
-    })
+    });
 
-    gulp.watch(['static/p/**/*.html', 'static/p/**/*.md', 'static/p/**/*.css'], gulp.series('reload'))
-})
+    gulp.watch(['static/p/**/*.html', 'static/p/**/*.md', 'static/p/**/*.css'], gulp.series('reload'));
+});
