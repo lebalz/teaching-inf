3481bc40bcc20f71a228010975020399125d6879
LeBalz <lebalz@outlook.com>
2024-08-21 08:44:38 +0200

    use useIsBrowser hook to react on mobx changes
    (cherry picked from commit 88af1ff8f8dc9676d053d347ef81c7c04d650a11)

--- src/models/User.ts
+++ src/models/User.ts
@@ -26,6 +26,23 @@ export default class User {
     }
 
     @computed
+    get isStudent() {
+        return /@edu/i.test(this.email);
+    }
+
+    get isTeacher() {
+        return !this.isStudent;
+    }
+
+    @computed
+    get nameShort() {
+        if (this.isStudent) {
+            return `${this.firstName} ${this.lastName.slice(0, 1)}.`;
+        }
+        return `${this.firstName.slice(0, 1)}. ${this.lastName}`;
+    }
+
+    @computed
     get props(): UserProps {
         return {
             id: this.id,
