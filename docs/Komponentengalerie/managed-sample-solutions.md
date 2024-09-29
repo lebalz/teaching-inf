377956abd9c66be7e337f271dcfd5906590d4be4
Silas Berger <silas.berger@outlook.com>
2024-09-29 13:58:59 +0200

    Work on first draft.

---
+++ docs/Komponentengalerie/managed-sample-solutions.md
@@ -0,0 +1,22 @@
+---
+page_id: f632a479-cfe4-4a98-848a-a0dd0e0f6535
+---
+
+import SampleSolutionTaskState from '@tdev-components/documents/SampleSolutionTaskState';
+import Solution from '@tdev-components/documents/Solution';
+import BrowserWindow from '@tdev-components/BrowserWindow';
+
+# Gesteuerte Musterlösungen
+
+<BrowserWindow>
+:::note[1. Aufgabe]
+<SampleSolutionTaskState id="df3313a5-c18f-4220-9dfe-cf4314c1b7b9" solutionId="e92b6f49-396e-48bc-8a6c-4ca94947210d">
+</SampleSolutionTaskState>
+
+Erstelle eine Lösung für die Aufgabe.
+
+<Solution id="e92b6f49-396e-48bc-8a6c-4ca94947210d">
+Hallo Welt 🌍
+</Solution>
+:::
+</BrowserWindow>
---
+++ src/components/documents/SampleSolutionTaskState/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { useFirstMainDocument } from '@tdev-hooks/useFirstMainDocument';
+import Loader from '@tdev-components/Loader';
+import TaskState from '@tdev-components/documents/TaskState';
+import { MetaInit, TaskMeta } from '@tdev-models/documents/TaskState';
+import { StateType } from '@tdev-api/document';
+import { ModelMeta as SolutionModelMeta } from '@tdev-models/documents/Solution';
+import { useDocumentRoot } from '@tdev-hooks/useDocumentRoot';
+import { NoneAccess } from '@tdev-models/helpers/accessPolicy';
+import { useStore } from '@tdev-hooks/useStore';
+
+interface Props extends MetaInit {
+    id: string;
+    solutionId: string;
+    keepSolution?: boolean;
+}
+
+const STATE_OPEN: StateType = 'unset';
+const STATE_WAITING_FOR_SOLUTION: StateType = 'star-empty';
+const STATE_REVIEWING_SOLUTION: StateType = 'star-half';
+const STATE_DONE: StateType = 'checked';
+
+const SampleSolutionTaskState = observer((props: Props) => {
+    const [taskMeta] = React.useState(new TaskMeta(props));
+    const [solutionMeta] = React.useState(new SolutionModelMeta({}))
+    const doc = useFirstMainDocument(props.id, taskMeta);
+    const solution = useFirstMainDocument(props.solutionId, solutionMeta);
+    const solutionDocRoot = useDocumentRoot(props.id, solutionMeta, false);
+    const userStore = useStore('userStore');
+
+    if (!doc) {
+        return <Loader />;
+    }
+
+    const solutionAvailable = !!solution && !NoneAccess.has(solutionDocRoot.permission);
+    console.log(solutionDocRoot, solutionAvailable);
+    // const solutionAvailable = !!solution && (!NoneAccess.has(solutionDocRoot.permission) || userStore.current?.isAdmin);
+
+    const taskStates = [
+        STATE_OPEN,
+        solutionAvailable ? STATE_REVIEWING_SOLUTION : STATE_WAITING_FOR_SOLUTION,
+        solutionAvailable ? STATE_DONE : null
+    ].filter(state => !!state);
+
+    if (solutionAvailable && doc.taskState === STATE_WAITING_FOR_SOLUTION) {
+        doc.setState(STATE_REVIEWING_SOLUTION);
+    } else if (!solutionAvailable && doc.taskState === STATE_REVIEWING_SOLUTION) {
+        doc.setState(STATE_WAITING_FOR_SOLUTION);
+    }
+
+    /*
+    if (doc.taskState === STATE_OPEN || (doc.taskState === STATE_DONE && !props.keepSolution)) {
+        console.log('I would set the solution\'s local visibility to None now...');
+    }
+
+     */
+
+    return (
+        <TaskState id={props.id} states={taskStates} />
+    );
+});
+
+export default SampleSolutionTaskState;
