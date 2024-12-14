7d2790140074fea84e17f8c38001e6f7c8987277
LeBalz <lebalz@outlook.com>
2024-12-14 17:06:53 +0100

    add some control buttons

--- src/components/shared/QR-Code/Scanner/index.tsx
+++ src/components/shared/QR-Code/Scanner/index.tsx
@@ -2,26 +2,43 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 import { observer } from 'mobx-react-lite';
-import { useFirstMainDocument } from '@tdev-hooks/useFirstMainDocument';
 import Loader from '@tdev-components/Loader';
-import { Scanner as QRScanner } from '@yudiel/react-qr-scanner';
+import { useQrScanner } from '@tdev-hooks/useQrScanner';
+import Button from '@tdev-components/shared/Button';
 
 interface Props {}
 const Scanner = observer((props: Props) => {
     const [qr, setQr] = React.useState('');
-    const [paused, setPaused] = React.useState(false);
+    const Lib = useQrScanner();
+    if (!Lib) {
+        return <Loader />;
+    }
     return (
         <div className={clsx('card', styles.qr)}>
             <div className={clsx(styles.scanner, 'card__body')}>
-                <QRScanner
-                    paused={paused}
+                <Lib.Scanner
+                    paused={!!qr}
                     onScan={(result) => {
-                        console.log(result);
-                        setPaused(true);
+                        setQr(result[0].rawValue);
                     }}
                     allowMultiple={false}
                 />
             </div>
+            {qr && (
+                <div className="card__footer">
+                    <small>{qr}</small>
+
+                    <div className="button-group button-group--block">
+                        <Button
+                            className={clsx('button--block')}
+                            text="Neu scannen"
+                            color="secondary"
+                            onClick={() => setQr('')}
+                        />
+                        <Button className={clsx('button--block')} color="primary" text="Besuchen" href={qr} />
+                    </div>
+                </div>
+            )}
         </div>
     );
 });
