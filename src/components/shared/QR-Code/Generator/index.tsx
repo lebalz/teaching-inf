7ceba0ea8537e1019d1706dff3820559173f6e28
LeBalz <lebalz@outlook.com>
2024-12-15 20:06:53 +0100

    use canvas

--- src/components/shared/QR-Code/Generator/index.tsx
+++ src/components/shared/QR-Code/Generator/index.tsx
@@ -2,17 +2,24 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 
-import { QRCodeSVG } from 'qrcode.react';
+import { QRCodeSVG, QRCodeCanvas } from 'qrcode.react';
 
 interface Props {
     content: string;
+    showText?: boolean;
 }
 const Generator = (props: Props) => {
+    const showFooter = props.showText;
     return (
         <div className={clsx('card', styles.qr)}>
             <div className={clsx(styles.generator, 'card__body')}>
-                <QRCodeSVG value={props.content} size={13 * 16} />
+                <QRCodeCanvas value={props.content} size={13 * 16} className={clsx(styles.qrImage)} />
             </div>
+            {showFooter && (
+                <div className={clsx(styles.footer, 'card__footer')}>
+                    <div className={clsx(styles.qrText)}>{props.content}</div>
+                </div>
+            )}
         </div>
     );
 };
