821ce5ba5877693bb81d1198d0864c5431742f4a
LeBalz <lebalz@outlook.com>
2025-02-21 21:17:06 +0100

    upload tab version

--- src/components/Cms/MdxEditor/plugins/plugins-jsx/InsertJsxOptions.tsx
+++ src/components/Cms/MdxEditor/plugins/plugins-jsx/InsertJsxOptions.tsx
@@ -1,6 +1,13 @@
-import { ButtonOrDropdownButton, insertJsx$, usePublisher } from '@mdxeditor/editor';
+import { ButtonOrDropdownButton, insertJsx$, insertMarkdown$, usePublisher } from '@mdxeditor/editor';
 import React from 'react';
-import { mdiApplicationOutline, mdiDotsVerticalCircleOutline, mdiFormatListCheckbox } from '@mdi/js';
+import {
+    mdiApplicationOutline,
+    mdiDotsVerticalCircleOutline,
+    mdiFormatListCheckbox,
+    mdiMathIntegral,
+    mdiMathIntegralBox,
+    mdiMathSin
+} from '@mdi/js';
 import Button from '@tdev-components/shared/Button';
 import Icon from '@mdi/react';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
@@ -13,45 +20,62 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
  */
 export const InsertJsxElements = () => {
     const insertJsx = usePublisher(insertJsx$);
+    const insertMdx = usePublisher(insertMarkdown$);
     const [editor] = useLexicalComposerContext();
 
     return (
-        <ButtonOrDropdownButton
-            items={[
-                {
-                    label: <Button icon={mdiFormatListCheckbox} text="DocCardList" iconSide="left" />,
-                    value: 'DocCardList'
-                },
-                {
-                    label: <Button icon={mdiApplicationOutline} text="BrowserWindow" iconSide="left" />,
-                    value: 'BrowserWindow'
-                }
-            ]}
-            title="Insert JSX Elements"
-            onChoose={(value) => {
-                setTimeout(() => {
-                    editor.update(() => {
-                        switch (value) {
-                            case 'DocCardList':
-                                insertJsx({
-                                    name: 'DocCardList',
-                                    kind: 'flow',
-                                    props: {}
-                                });
-                                break;
-                            case 'BrowserWindow':
-                                insertJsx({
-                                    name: 'BrowserWindow',
-                                    kind: 'flow',
-                                    props: {}
-                                });
-                                break;
-                        }
-                    });
-                }, 1);
-            }}
-        >
-            <Icon path={mdiDotsVerticalCircleOutline} size={1} />
-        </ButtonOrDropdownButton>
+        <>
+            <Button
+                icon={mdiMathIntegral}
+                title="InlineMath"
+                onClick={() => {
+                    insertMdx('$\\LaTeX$');
+                }}
+            />
+            <ButtonOrDropdownButton
+                items={[
+                    {
+                        label: <Button icon={mdiFormatListCheckbox} text="DocCardList" iconSide="left" />,
+                        value: 'DocCardList'
+                    },
+                    {
+                        label: <Button icon={mdiApplicationOutline} text="BrowserWindow" iconSide="left" />,
+                        value: 'BrowserWindow'
+                    },
+                    {
+                        label: <Button icon={mdiMathIntegralBox} text="Math-Block" iconSide="left" />,
+                        value: 'Math'
+                    }
+                ]}
+                title="Insert JSX Elements"
+                onChoose={(value) => {
+                    setTimeout(() => {
+                        editor.update(() => {
+                            switch (value) {
+                                case 'DocCardList':
+                                    insertJsx({
+                                        name: 'DocCardList',
+                                        kind: 'flow',
+                                        props: {}
+                                    });
+                                    break;
+                                case 'BrowserWindow':
+                                    insertJsx({
+                                        name: 'BrowserWindow',
+                                        kind: 'flow',
+                                        props: {}
+                                    });
+                                    break;
+                                case 'Math':
+                                    insertMdx('$$\n\\LaTeX\n$$');
+                                    break;
+                            }
+                        });
+                    }, 1);
+                }}
+            >
+                <Icon path={mdiDotsVerticalCircleOutline} size={1} />
+            </ButtonOrDropdownButton>
+        </>
     );
 };
