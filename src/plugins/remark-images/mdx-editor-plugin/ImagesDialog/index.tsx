67f229732e1093b0b352e2412815791ed5782385
LeBalz <lebalz@outlook.com>
2025-02-26 14:23:27 +0100

    cleanup

--- src/plugins/remark-images/mdx-editor-plugin/ImagesDialog/index.tsx
+++ src/plugins/remark-images/mdx-editor-plugin/ImagesDialog/index.tsx
@@ -1,25 +1,16 @@
-/**
- * By Mdx Editor, @url https://github.com/mdx-editor/editor/tree/main/src/plugins/image
- */
-
 import React from 'react';
-// import styles from '../../styles/ui.module.css';
 import Card from '@tdev-components/shared/Card';
 import styles from './styles.module.scss';
 import { observer } from 'mobx-react-lite';
-import ImageGallery from './ImageGallery';
 import { usePublisher } from '@mdxeditor/editor';
 import { insertImage$ } from '..';
 import TextInput from '@tdev-components/shared/TextInput';
-import ImagePreview from '@tdev-components/Cms/Github/iFile/File/FilePreview/ImagePreview';
 import Button from '@tdev-components/shared/Button';
 import FileUpload from '@tdev-components/shared/FileUpload';
 import clsx from 'clsx';
 import { Asset } from '@tdev-models/cms/Dir';
 import BinFile from '@tdev-models/cms/BinFile';
 import AssetSelector from '@tdev-components/Cms/MdxEditor/AssetSelector';
-import FileStub from '@tdev-models/cms/FileStub';
-import iFile from '@tdev-models/cms/iFile';
 import { useStore } from '@tdev-hooks/useStore';
 
 interface Props {
@@ -82,7 +73,6 @@ export const ImageDialog = observer((props: Props) => {
         >
             <AssetSelector
                 onSelect={(selected) => {
-                    console.log('Selected', selected.path, file?.relativePath(selected));
                     const src = activeEntry.relativePath(selected);
                     setSrc(src);
                     setFile(selected as BinFile);
