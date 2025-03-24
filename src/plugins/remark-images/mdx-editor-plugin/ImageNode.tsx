f7e9d0ca72e4ebbb3062b1ccddf1407bd6857032
LeBalz <lebalz@outlook.com>
2025-03-24 16:18:46 +0100

    ensure image nodes do update

--- src/plugins/remark-images/mdx-editor-plugin/ImageNode.tsx
+++ src/plugins/remark-images/mdx-editor-plugin/ImageNode.tsx
@@ -38,8 +38,6 @@ export type SerializedImageNode = Spread<
 export class ImageNode extends DecoratorNode<React.ReactNode> {
     /** @internal */
     __src: string;
-    /** @internal */
-    __width?: number;
 
     __options: ParsedOptions;
 
@@ -50,7 +48,7 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
 
     /** @internal */
     static clone(node: ImageNode): ImageNode {
-        return new ImageNode(node.__src, node.getAltText(), node.__key);
+        return new ImageNode(node.getSrc(), node.getAltText(), node.getKey());
     }
 
     /** @internal */
@@ -72,7 +70,6 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
         this.__src = src;
         const opts = parseOptions(altText, true);
         this.__options = opts;
-        this.__width = (opts as any).width || 200;
     }
 
     /** @internal */
@@ -86,15 +83,12 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
     }
 
     setWidth(width: number | undefined): void {
-        this.getWritable().__width = width;
+        this.getWritable().__options.width = width;
     }
 
     setOptions(options: { name: string; value: number | string | undefined }[]) {
         const newOptions: Record<string, string | number> = {};
         options.forEach((option) => {
-            if (option.name === 'width') {
-                return this.setWidth(option.value as number);
-            }
             if (!option.value || option.value === 'false') {
                 delete newOptions[camelCased(option.name)];
                 return;
@@ -105,7 +99,8 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
     }
 
     getAltText(): string {
-        const alt = serializeOptions({ ...this.__options, width: this.__width });
+        const latest = this.getLatest();
+        const alt = serializeOptions({ ...latest.__options });
         return alt;
     }
 
@@ -120,7 +115,7 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
     }
 
     getSrc(): string {
-        return this.__src;
+        return this.getLatest().__src;
     }
 
     setSrc(src: string): void {
@@ -129,7 +124,14 @@ export class ImageNode extends DecoratorNode<React.ReactNode> {
 
     /** @internal */
     decorate(_parentEditor: LexicalEditor): React.ReactNode {
-        return <ImageComponent src={this.getSrc()} nodeKey={this.getKey()} width={this.__width} />;
+        console.log('ImageNode.decorate', this.getKey(), this.__options);
+        return (
+            <ImageComponent
+                src={this.getSrc()}
+                nodeKey={this.getKey()}
+                options={this.getLatest().__options}
+            />
+        );
     }
 }
 
