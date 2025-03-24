a938b2ef7841f12804f3536944a9a89cd5334fcf
LeBalz <lebalz@outlook.com>
2025-03-24 14:15:58 +0100

    add some math exercises

---
+++ src/components/documents/String/sanitizers.ts
@@ -0,0 +1,13 @@
+export const toFixedNum = (num: number, to: number = 3) => {
+    const precision = Math.pow(10, to);
+    return (Math.round(num * precision) / precision).toString();
+};
+
+export const sanitizeNumber = (excludedText: string[] = []) => {
+    const replacer = (text: string) => {
+        return excludedText.reduce((acc, curr) => acc.replaceAll(curr, ''), text);
+    };
+    return (solution: string) => {
+        return replacer(`${solution}`.replace(',', '.')).replaceAll(/[^\d\.-]/g, '');
+    };
+};
