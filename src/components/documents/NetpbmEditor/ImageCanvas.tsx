f84fe743802dbf976f61cc39eda6ed6a7fb51fd8
LeBalz <lebalz@outlook.com>
2025-04-15 16:25:42 +0200

    add download as png

--- src/components/documents/NetpbmEditor/ImageCanvas.tsx
+++ src/components/documents/NetpbmEditor/ImageCanvas.tsx
@@ -1,29 +1,23 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './styles.module.scss';
 import clsx from 'clsx';
+import Button from '@tdev-components/shared/Button';
+import { mdiImage, mdiImageSizeSelectLarge } from '@mdi/js';
+import { SIZE_S } from '@tdev-components/shared/iconSizes';
+import { createDownloadCanvas } from './createDownloadCanvas';
 
 interface Props {
     pixels?: Uint8ClampedArray;
     width: number;
     height: number;
+    format: 'P1' | 'P2' | 'P3';
+    extension: 'pbm' | 'pgm' | 'ppm';
+    actionsClassName?: string;
 }
 
-const ImageCanvas = ({ width, height, pixels }: Props) => {
+const ImageCanvas = (props: Props) => {
+    const { width, height, pixels } = props;
     const canvasRef = useRef<HTMLCanvasElement>(null);
-
-    const nPixels = width * height;
-    const scale =
-        nPixels < 400
-            ? 50
-            : nPixels < 1600
-              ? 25
-              : nPixels < 6400
-                ? 10
-                : nPixels < 14400
-                  ? 5
-                  : nPixels < 84800
-                    ? 2
-                    : 1;
     const offsetX = 0;
     const offsetY = 0;
 
@@ -45,6 +39,8 @@ const ImageCanvas = ({ width, height, pixels }: Props) => {
 
         // Get device pixel ratio
         const dpr = window.devicePixelRatio || 1;
+        const scale = Math.min(50, Math.max(1, Math.floor(window.innerWidth / width)));
+
         // Set display size (css pixels)
         const displayWidth = width * scale;
         const displayHeight = height * scale;
@@ -76,9 +72,51 @@ const ImageCanvas = ({ width, height, pixels }: Props) => {
         ctx.clearRect(0, 0, displayWidth, displayHeight);
         ctx.imageSmoothingEnabled = false;
         ctx.drawImage(tempCanvas, offsetX, offsetY, displayWidth, displayHeight);
-    }, [pixels, width, height, scale, offsetX, offsetY]);
+    }, [pixels, width, height, offsetX, offsetY]);
 
-    return <canvas className={clsx(styles.canvas, { [styles.hidden]: hidden() })} ref={canvasRef} />;
+    const onDownload = React.useCallback(
+        (size: 'original' | 'scaled') => {
+            if (!canvasRef.current) {
+                return;
+            }
+            const dWidth = size === 'original' ? width : canvasRef.current.width;
+            const dHeight = size === 'original' ? height : canvasRef.current.height;
+            const canvas = createDownloadCanvas(canvasRef.current, { width: dWidth, height: dHeight });
+            if (!canvas) {
+                return;
+            }
+            // Create download link
+            const link = document.createElement('a');
+            link.download = `${props.format}_${width}x${height}${size === 'original' ? '' : `_zoomfaktor_${Math.round((10 * dWidth) / width) / 10}`}.${props.extension}.png`;
+            link.href = canvas.toDataURL('image/png');
+            link.click();
+        },
+        [width, height, canvasRef]
+    );
+
+    return (
+        <>
+            <canvas className={clsx(styles.canvas, { [styles.hidden]: hidden() })} ref={canvasRef} />
+            {canvasRef.current && width > 0 && height > 0 && (
+                <div className={clsx(props.actionsClassName)}>
+                    <Button
+                        color="primary"
+                        icon={mdiImage}
+                        size={SIZE_S}
+                        onClick={() => onDownload('scaled')}
+                        title={`Bild als PNG herunterladen (gross, ${canvasRef.current?.width}x${canvasRef.current?.height}px)`}
+                    />
+                    <Button
+                        color="primary"
+                        icon={mdiImageSizeSelectLarge}
+                        size={SIZE_S}
+                        onClick={() => onDownload('original')}
+                        title={`Original-Bild als PNG herunterladen (klein, ${width}x${height}px)`}
+                    />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ImageCanvas;
