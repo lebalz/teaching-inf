456b1ba36c3ca40c056e26a20eede9ab7790a0aa
LeBalz <lebalz@outlook.com>
2025-04-30 08:40:23 +0200

    improve template replacer

--- src/components/DynamicValues/templateReplacer.ts
+++ src/components/DynamicValues/templateReplacer.ts
@@ -6,17 +6,5 @@ export const templateReplacer = (code?: string, dynamicValues?: ObservableMap<st
     if (!code) {
         return '';
     }
-    let replaced = code;
-    let iterations = 0;
-
-    while (true) {
-        const match = replaced.match(KeyRegex);
-        if (!match || !match.groups || iterations > 50) {
-            break;
-        }
-        iterations++;
-        const key = match.groups.key;
-        replaced = replaced.replaceAll(`{{${key}}}`, dynamicValues?.get(key) || `<${key}>`);
-    }
-    return replaced;
+    return code.replace(KeyRegex, (_, key) => dynamicValues?.get(key) || `<${key}>`);
 };
