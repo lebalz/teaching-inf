b3a1a5c7e8d8fe21c84a2527012e4dfc4e9ff77e
Silas Berger <silas.berger@outlook.com>
2025-05-18 12:36:53 +0200

    Format.

--- src/components/StudentGroup/AddUserPopup.tsx
+++ src/components/StudentGroup/AddUserPopup.tsx
@@ -19,60 +19,60 @@ interface Props {
 }
 
 const AddIndividualUsersPopup = observer((props: Props) => {
-        const userStore = useStore('userStore');
-        const [searchFilter, setSearchFilter] = React.useState('');
-        const [searchRegex, setSearchRegex] = React.useState(new RegExp(searchFilter, 'i'));
+    const userStore = useStore('userStore');
+    const [searchFilter, setSearchFilter] = React.useState('');
+    const [searchRegex, setSearchRegex] = React.useState(new RegExp(searchFilter, 'i'));
 
-        React.useEffect(() => {
-            setSearchRegex(new RegExp(searchFilter, 'i'));
-        }, [searchFilter]);
+    React.useEffect(() => {
+        setSearchRegex(new RegExp(searchFilter, 'i'));
+    }, [searchFilter]);
 
-        const group = props.studentGroup;
+    const group = props.studentGroup;
 
     return (
         <>
             <div className={clsx('card__header', styles.header)}>
-                    <h3>Benutzer:in hinzufügen</h3>
+                <h3>Benutzer:in hinzufügen</h3>
+            </div>
+            <div className={clsx('card__body')}>
+                <input
+                    type="text"
+                    placeholder="Suche..."
+                    value={searchFilter}
+                    className={clsx(styles.textInput)}
+                    onChange={(e) => {
+                        setSearchFilter(e.target.value);
+                    }}
+                />
+                <div className={styles.listContainer}>
+                    <ul className={clsx(styles.students, styles.list)}>
+                        {userStore.users
+                            .filter((user) => searchRegex.test(user.searchTerm))
+                            .map((user, idx) => (
+                                <li
+                                    key={idx}
+                                    className={clsx(
+                                        styles.listItem,
+                                        group.userIds.has(user.id) && styles.disabled
+                                    )}
+                                    title={user.email}
+                                >
+                                    {user.nameShort}
+                                    <div className={styles.actions}>
+                                        <Button
+                                            onClick={() => {
+                                                group.addStudent(user);
+                                            }}
+                                            disabled={group.userIds.has(user.id)}
+                                            icon={mdiAccountPlus}
+                                            color="green"
+                                        />
+                                    </div>
+                                </li>
+                            ))}
+                    </ul>
                 </div>
-                <div className={clsx('card__body')}>
-                    <input
-                        type="text"
-                        placeholder="Suche..."
-                        value={searchFilter}
-                        className={clsx(styles.textInput)}
-                        onChange={(e) => {
-                            setSearchFilter(e.target.value);
-                        }}
-                    />
-                    <div className={styles.listContainer}>
-                        <ul className={clsx(styles.students, styles.list)}>
-                            {userStore.users
-                                .filter((user) => searchRegex.test(user.searchTerm))
-                                .map((user, idx) => (
-                                    <li
-                                        key={idx}
-                                        className={clsx(
-                                            styles.listItem,
-                                            group.userIds.has(user.id) && styles.disabled
-                                        )}
-                                        title={user.email}
-                                    >
-                                        {user.nameShort}
-                                        <div className={styles.actions}>
-                                            <Button
-                                                onClick={() => {
-                                                    group.addStudent(user);
-                                                }}
-                                                disabled={group.userIds.has(user.id)}
-                                                icon={mdiAccountPlus}
-                                                color="green"
-                                            />
-                                        </div>
-                                    </li>
-                                ))}
-                        </ul>
-                    </div>
-                </div>
+            </div>
         </>
     );
 });
