f1a7e5620a8a160ddf8ab79af3c5f73d031194d4
bh0fer <lebalz@outlook.com>
2025-10-26 20:49:11 +0100

    add unlisted pseudoclass document

---
+++ docs/WMS-WINF/03-css/02-pseudoklassen/assets/GridDemo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import clsx from 'clsx';
+
+const GridDemo = () => {
+    return (
+        <div className={clsx(styles.main)}>
+            {Array.from({ length: 9 }, (_, i) => (
+                <div key={i + 1}>{i + 1}</div>
+            ))}
+        </div>
+    )
+}
+
+export default GridDemo;
