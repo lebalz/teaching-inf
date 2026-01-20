1469c7804bf81a430ef5b8f354ba2bc91153fccc
bh0fer <lebalz@outlook.com>
2026-01-20 10:54:29 +0100

    fix

--- website/stores/ToolsStore/ClockStore.ts
+++ website/stores/ToolsStore/ClockStore.ts
@@ -1,53 +1,5 @@
+import { Clock } from '@tdev/packages/pyodide-code/models/Clock';
 import { action, computed, observable } from 'mobx';
-
-export class Clock {
-    /** hours, minutes and seconds ALWAYS in degrees */
-    @observable accessor hours: number = 0;
-    @observable accessor minutes: number = 0;
-    @observable accessor seconds: number = 0;
-    @observable accessor animationSpeed = 0;
-
-    @computed
-    get transitionDurationMs() {
-        if (this.animationSpeed <= 0) {
-            return 0;
-        }
-        return 1000 / this.animationSpeed;
-    }
-
-    @action
-    setAnimationSpeed(speed: number) {
-        this.animationSpeed = speed;
-    }
-
-    @action
-    reset() {
-        this.setClock(0, 0, 0);
-    }
-
-    @action
-    setHours(deg: number) {
-        this.hours = deg;
-    }
-
-    @action
-    setMinutes(deg: number) {
-        console.log('Setting minutes to', deg);
-        this.minutes = deg;
-    }
-
-    @action
-    setSeconds(deg: number) {
-        this.seconds = deg;
-    }
-
-    @action
-    setClock(hourDeg: number, minutesDeg: number, secondsDeg: number) {
-        this.setHours(hourDeg);
-        this.setMinutes(minutesDeg);
-        this.setSeconds(secondsDeg);
-    }
-}
 
 class ClockStore {
     @observable.ref accessor defaultClock = new Clock();
