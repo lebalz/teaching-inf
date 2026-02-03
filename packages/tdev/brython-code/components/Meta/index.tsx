44c058eeb43fb9c652f6c4f51910475093ff3131
bh0fer <lebalz@outlook.com>
2026-02-03 22:47:41 +0100

    fix scroll offset

--- packages/tdev/brython-code/components/Meta/index.tsx
+++ packages/tdev/brython-code/components/Meta/index.tsx
@@ -12,22 +12,43 @@ interface Props {
     code: Script;
 }
 
+function getNearestScrollParent(el: HTMLElement | null): HTMLElement | null {
+    let parent = el?.parentElement;
+    while (parent && parent !== document.body) {
+        const style = window.getComputedStyle(parent);
+        if (/(auto|scroll|overlay)/.test(style.overflowY) && parent.scrollHeight > parent.clientHeight) {
+            return parent;
+        }
+        parent = parent.parentElement;
+    }
+    return null;
+}
+
 const Meta = observer((props: Props) => {
     const { code } = props;
     const pageStore = useStore('pageStore');
+    const ref = React.useRef<HTMLDivElement>(null);
+    const parentRef = React.useMemo(() => {
+        if (ref.current && pageStore.runningTurtleScriptId === code.id) {
+            return getNearestScrollParent(ref.current);
+        }
+        return null;
+    }, [ref, code.graphicsModalExecutionNr, code, pageStore]);
     if (code.lang !== 'python') {
         return null;
     }
     return (
         <>
-            <div id={DOM_ELEMENT_IDS.outputDiv(code.codeId)}></div>
+            <div id={DOM_ELEMENT_IDS.outputDiv(code.codeId)} ref={ref}></div>
             {code.graphicsModalExecutionNr > 0 && (
                 <>
                     {code.hasTurtleOutput && pageStore.runningTurtleScriptId === code.id && (
-                        <Turtle code={code} />
+                        <Turtle code={code} scrollOffsetY={parentRef?.scrollTop} />
                     )}
-                    {code.hasCanvasOutput && <Canvas code={code} />}
-                    {!code.hasCanvasOutput && !code.hasTurtleOutput && <Graphics code={code} />}
+                    {code.hasCanvasOutput && <Canvas code={code} scrollOffsetY={parentRef?.scrollTop} />}
+                    {!code.hasCanvasOutput && !code.hasTurtleOutput && (
+                        <Graphics code={code} scrollOffsetY={parentRef?.scrollTop} />
+                    )}
                 </>
             )}
             {code.lang === 'python' && <BrythonCommunicator code={code} />}
