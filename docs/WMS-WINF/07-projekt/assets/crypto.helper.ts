5f5dc9d2d34765c53386d48c99903b983c8c1c5e
bh0fer <lebalz@outlook.com>
2026-02-25 00:08:46 +0100

    add deploy settings

---
+++ docs/WMS-WINF/07-projekt/assets/crypto.helper.ts
@@ -0,0 +1,12 @@
+export const createRandomString = () => {
+    const length = 64;
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:',.<>/?`~\\ ";
+    let result = '';
+    const randomArray = new Uint8Array(length);
+    crypto.getRandomValues(randomArray);
+    randomArray.forEach((number) => {
+        result += chars[number % chars.length];
+    });
+    return result;
+};
