02d889fdf5829a827042c5c64225e8d397a1a1be
bh0fer <lebalz@outlook.com>
2026-08-05 21:12:53 +0200

    [tdev] move migration tools to package.

--- packages/tdev/material-sync/src/migrate_tdev/helpers/gitHelpers.ts
+++
@@ -1,6 +0,0 @@
-import { execa } from 'execa';
-
-export const hasUncommittedChanges = async (): Promise<boolean> => {
-    const { stdout } = await execa`git status --porcelain`;
-    return stdout.trim() !== '';
-};
